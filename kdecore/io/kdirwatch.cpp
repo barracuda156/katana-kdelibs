4c861f57e46ee8be2731246f73e39779a3f1b8e2
Ivailo Monev <xakepa10@gmail.com>
2023-11-05 00:21:56 +0200

    kdecore: remove compat bits
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdecore/io/kdirwatch.cpp
+++ kdecore/io/kdirwatch.cpp
@@ -121,21 +121,12 @@ bool KDirWatch::contains(const QString &path) const
 
 int KDirWatch::interval() const
 {
-#if QT_VERSION < 0x041400
-    // same as the default of Katie
-    return 1000;
-#else
     return d->watcher->interval();
-#endif
 }
 
 void KDirWatch::setInterval(int interval)
 {
-#if QT_VERSION < 0x041400
-    Q_UNUSED(interval);
-#else
     d->watcher->setInterval(interval);
-#endif
 }
 
 void KDirWatch::setDirty(const QString &file)
