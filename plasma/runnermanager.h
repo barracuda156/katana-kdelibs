bd3425b18724774cf9ff2a87ef84c50662628ed0
Ivailo Monev <xakepa10@gmail.com>
2024-04-18 22:23:23 +0300

    plasma: Plasma::RunnerManager documentation update
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- plasma/runnermanager.h
+++ plasma/runnermanager.h
@@ -41,130 +41,129 @@ namespace Plasma
 /**
  * @class RunnerManager plasma/runnermanager.h <Plasma/RunnerManager>
  *
- * @short The RunnerManager class decides what installed runners are runnable,
- *        and their ratings. It is the main proxy to the runners.
+ * @short The RunnerManager class is a proxy to the runners.
  */
 class PLASMA_EXPORT RunnerManager : public QObject
 {
     Q_OBJECT
+public:
+    explicit RunnerManager(QObject *parent=0);
+    ~RunnerManager();
 
-    public:
-        explicit RunnerManager(QObject *parent=0);
-        ~RunnerManager();
+    /**
+     * Finds and returns a loaded runner or null
+     *
+     * @param name the name of the runner
+     * @return Pointer to the runner
+     */
+    AbstractRunner* runner(const QString &name) const;
 
-        /**
-         * Finds and returns a loaded runner or NULL
-         * @param name the name of the runner
-         * @return Pointer to the runner
-         */
-        AbstractRunner* runner(const QString &name) const;
+    /**
+     * @return the list of all currently loaded runners
+     */
+    QList<AbstractRunner*> runners() const;
 
-        /**
-         * @return the list of all currently loaded runners
-         */
-        QList<AbstractRunner*> runners() const;
+    /**
+     * Retrieves the current context
+     *
+     * @return pointer to the current context
+     */
+    RunnerContext *searchContext() const;
 
-        /**
-         * Retrieves the current context
-         * @return pointer to the current context
-         */
-        RunnerContext *searchContext() const;
+    /**
+     * Retrieves all available matches found so far for the launched query
+     *
+     * @return List of matches
+     */
+    QList<QueryMatch> matches() const;
 
-        /**
-         * Retrieves all available matches found so far for the previously launched query
-         * @return List of matches
-         */
-        QList<QueryMatch> matches() const;
+    /**
+     * Retrieves the list of actions, if any, for a match
+     */
+    QList<QAction*> actionsForMatch(const QueryMatch &match);
 
-        /**
-         * Retrieves the list of actions, if any, for a match
-         */
-        QList<QAction*> actionsForMatch(const QueryMatch &match);
+    /**
+     * @return the current query term
+     */
+    QString query() const;
 
-        /**
-         * @return the current query term
-         */
-        QString query() const;
+    /**
+     * Sets a whitelist for the plugins that can be loaded, any runners not in the list are not
+     * loaded and used for matching.
+     *
+     * @param plugins the plugin names of allowed runners
+     * @since 4.4
+     */
+    void setAllowedRunners(const QStringList &runners);
 
-        /**
-         * Sets a whitelist for the plugins that can be loaded
-         *
-         * @param plugins the plugin names of allowed runners
-         * @since 4.4
-         */
-        void setAllowedRunners(const QStringList &runners);
+    /**
+     * Attempts to add the AbstractRunner plugin represented by the KService passed in. Use in
+     * cases where specific runners should be loaded.
+     *
+     * @param service the service to use to load the plugin
+     * @since 4.5
+     */
+    void loadRunner(const KService::Ptr service);
 
-        /**
-         * Attempts to add the AbstractRunner plugin represented
-         * by the KService passed in. Usually one can simply let
-         * the configuration of plugins handle loading Runner plugins,
-         * but in cases where specific runners should be loaded this
-         * allows for that to take place
-         *
-         * @param service the service to use to load the plugin
-         * @since 4.5
-         */
-        void loadRunner(const KService::Ptr service);
+    /**
+     * @return the list of allowed plugins
+     * @since 4.4
+     */
+    QStringList allowedRunners() const;
 
-        /**
-         * @return the list of allowed plugins
-         * @since 4.4
-         */
-        QStringList allowedRunners() const;
+    /**
+     * @return mime data of the specified match
+     * @since 4.5
+     */
+    QMimeData* mimeDataForMatch(const QueryMatch &match) const;
 
-        /**
-         * @return mime data of the specified match
-         * @since 4.5
-         */
-        QMimeData* mimeDataForMatch(const QueryMatch &match) const;
+    /**
+     * Returns a list of all known Runner implementations
+     *
+     * @param parentApp the application to filter applets on. Uses the X-KDE-ParentApp entry
+     *                  (if any) in the plugin info. The default value of QString() will result
+     *                  in a list containing only applets not specifically registered to an
+     *                  application.
+     * @return list of AbstractRunners
+     * @since 4.6
+     **/
+    static KPluginInfo::List listRunnerInfo(const QString &parentApp = QString());
 
-        /**
-         * Returns a list of all known Runner implementations
-         *
-         * @param parentApp the application to filter applets on. Uses the
-         *                  X-KDE-ParentApp entry (if any) in the plugin info.
-         *                  The default value of QString() will result in a
-         *                  list containing only applets not specifically
-         *                  registered to an application.
-         * @return list of AbstractRunners
-         * @since 4.6
-         **/
-        static KPluginInfo::List listRunnerInfo(const QString &parentApp = QString());
+public Q_SLOTS:
+    /**
+     * Launch a query, this will create threads and return inmediately. When matches are found,
+     * if any, matchesChanged signal is emitted.
+     *
+     * @param term the term we want to find matches for
+     */
+    void launchQuery(const QString &term);
 
-    public Q_SLOTS:
-        /**
-         * Launch a query, this will create threads and return inmediately.
-         * When the information will be available can be known using the
-         * matchesChanged signal.
-         *
-         * @param term the term we want to find matches for
-         */
-        void launchQuery(const QString &term);
+    /**
+     * Reset the current data and stops the query, will block until all active runners are
+     * finished (if any).
+     */
+    void reset();
 
-        /**
-         * Reset the current data and stops the query
-         */
-        void reset();
+Q_SIGNALS:
+    /**
+     * Emitted each time a new match is added to the list. The signal may
+     * not be emitted if not matches are found.
+     */
+    void matchesChanged(const QList<Plasma::QueryMatch> &matches);
 
-    Q_SIGNALS:
-        /**
-         * Emitted each time a new match is added to the list
-         */
-        void matchesChanged(const QList<Plasma::QueryMatch> &matches);
+    /**
+     * Emitted when the query finishes.
+     * @since 4.5
+     */
+    void queryFinished();
 
-        /**
-         * Emitted when the launchQuery finish
-         * @since 4.5
-         */
-        void queryFinished();
+private:
+    Q_PRIVATE_SLOT(d, void _k_checkFinished())
+    Q_PRIVATE_SLOT(d, void _k_matchesChanged())
 
-    private:
-        Q_PRIVATE_SLOT(d, void _k_checkFinished())
-        Q_PRIVATE_SLOT(d, void _k_matchesChanged())
+    RunnerManagerPrivate * const d;
 
-        RunnerManagerPrivate * const d;
-
-        friend class RunnerManagerPrivate;
+    friend class RunnerManagerPrivate;
 };
 
 }
