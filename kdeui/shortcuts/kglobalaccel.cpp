70feecedcf0d23e30ae0506357273268e625c394
Ivailo Monev <xakepa10@gmail.com>
2024-04-27 04:06:41 +0300

    kdeui: add TODO for global shorcuts stealing
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdeui/shortcuts/kglobalaccel.cpp
+++ kdeui/shortcuts/kglobalaccel.cpp
@@ -288,6 +288,7 @@ void KGlobalAccel::stealShortcutSystemwide(const QKeySequence &seq)
 {
     foreach (const KGlobalAccelStruct &shortcut, d->filter->shortcuts) {
         if (shortcut.action->globalShortcut().matches(seq) != QKeySequence::NoMatch) {
+            // TODO: in case of partial match this can steal only the matching one
             shortcut.action->setGlobalShortcut(QKeySequence());
             d->remove(shortcut.action);
             break;
