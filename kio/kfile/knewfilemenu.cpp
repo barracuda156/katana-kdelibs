c4eb95672ef1fbdc5c457df52c0fcf835ef7612a
Ivailo Monev <xakepa10@gmail.com>
2024-05-03 09:38:02 +0300

    kio: pass the device to KMimeType::findByNameAndContent()
    it reads more than 1024 because it is unknown how much data the rules need
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kio/kfile/knewfilemenu.cpp
+++ kio/kfile/knewfilemenu.cpp
@@ -527,7 +527,7 @@ void KNewFileMenuPrivate::executeStrategy()
             QFile srcFile(uSrc.toLocalFile());
             if (srcFile.open(QIODevice::ReadOnly)) {
                 KMimeType::Ptr wantedMime = KMimeType::findByUrl(uSrc);
-                KMimeType::Ptr mime = KMimeType::findByNameAndContent(m_copyData.m_chosenFileName, srcFile.read(1024));
+                KMimeType::Ptr mime = KMimeType::findByNameAndContent(m_copyData.m_chosenFileName, &srcFile);
                 // kDebug() << "mime=" << mime->name() << "wantedMime=" << wantedMime->name();
                 if (!mime->is(wantedMime->name()))
                     chosenFileName += wantedMime->mainExtension();
