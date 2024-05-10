b46e8b1170d778435d5b69d4a2a7aaf6b819de97
Ivailo Monev <xakepa10@gmail.com>
2024-05-10 09:14:09 +0300

    solid: remove unused SOLID_CALL() macro
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- solid/solid/soliddefs_p.h
+++ solid/solid/soliddefs_p.h
@@ -34,13 +34,4 @@
          return Default; \
     }
 
-
-
-#define SOLID_CALL(Type, Object, Method) \
-    Type t = qobject_cast<Type>(Object); \
-    if (t!=0) \
-    { \
-         t->Method; \
-    }
-
 #endif
